1fc9284903cdd6dec55ba67afc6ac3718f39509f
akbalixdj8889-eng <akbalixdj8889@gmail.com>
2026-03-20 15:28:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -148,8 +148,7 @@ if 'paso' not in st.session_state:
 
 
 def enviar_a_google(nombre, curso, mision, aciertos, powerup):
-    url_script = 
-    "https://script.google.com/macros/s/AKfycbyX5JRshORtaiXVXTXF73nblLEj3M4oX79hF_heKDbEHnuPwfH0PqJBTDXM8_gqYHx4cQ/exec"
+    url_script ="https://script.google.com/macros/s/AKfycbyX5JRshORtaiXVXTXF73nblLEj3M4oX79hF_heKDbEHnuPwfH0PqJBTDXM8_gqYHx4cQ/exec"
     datos = {
         "nombre": nombre,
         "curso": curso,
